80d4dcd2c5327fd3444210814b9c72ed64ef5eed
Harshit <hvootukuri@gmail.com>
2025-06-01 20:36:16 +0530

    feat(cli): implemented basic command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version="1.0.0", about="A Terminal Gmail Client.", long_about = None)]
+struct CLI {
+    #[command(subcommand)] 
+    command: Option<Commands>,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Subcommand)]
+enum Commands {
+    /// login to a gmail account
+    login {
+        #[arg(short, long)]
+        email: String,
+        #[arg(short, long)]
+        password: String,
+    },
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = CLI::parse();
+    
 }
